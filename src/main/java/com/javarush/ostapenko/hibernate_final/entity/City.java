1e1e4cad4e1ef9d7171a20abe012db2da6168779
Darfresor <vectnik@gmail.com>
2026-01-08 17:27:15 +0300

    подгрузку страны из onetoone делаем lazy

--- src/main/java/com/javarush/ostapenko/hibernate_final/entity/City.java
+++ src/main/java/com/javarush/ostapenko/hibernate_final/entity/City.java
@@ -17,7 +17,7 @@ public class City {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "country_id")
     private Country countryId;
     private String district;
